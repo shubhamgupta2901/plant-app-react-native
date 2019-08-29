82d2a71482f1a3fa03acf4c530a1070392f3cadc
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-08-29 23:07:31 +0530

    Added header configuration for navigation

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createAppContainer, createStackNavigator} from 'react-navigation';
-import {Image} from 'react-native';
+import {Platform, Image} from 'react-native';
+
 
 import WelcomeScreen from '../screens/WelcomeScreen';
 import LoginScreen from '../screens/LoginScreen';
@@ -11,6 +12,7 @@ import ProductsScreen from '../screens/ProductsScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
 import {theme} from '../constants';
+
 
 const screens = createStackNavigator(
     {
@@ -23,13 +25,26 @@ const screens = createStackNavigator(
         settings: SettingsScreen,
     },
     {
-        initialRouteName: "welcome",
+        
         defaultNavigationOptions: {
-            headerStyle: {},
-            headerBackImage: <Image/>,
+            headerStyle: {
+                height: theme.sizes.base*4,
+                backgroundColor: theme.colors.white,
+                borderBottomColor: "transparent",
+                elevation: 0 //for android devices. Disabling the elevation of header
+            },
+            headerBackImage: <Image source={require('../../assets/icons/back.png')}/>,
             headerBackTitle: null,
-            headerLeftContainerStyle: {},
-            headerRightContainerStyle: {},
+            headerLeftContainerStyle: {
+                alignItems: 'center',
+                marginLeft: Platform.OS === 'ios' ? theme.sizes.base : 0,
+                padding: theme.sizes.base,
+            },
+            headerRightContainerStyle: {
+                alignItems: 'center',
+                marginLeft: Platform.OS === 'ios' ? theme.sizes.base : 0,
+                padding: theme.sizes.base,
+            },
         }
     }
 );
