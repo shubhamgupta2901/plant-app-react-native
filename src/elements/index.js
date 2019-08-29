341d51b0d9801a62847eb89ff3612b73be662151
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-08-29 22:11:26 +0530

    Added Input Element

--- src/elements/index.js
+++ src/elements/index.js
@@ -1,9 +1,11 @@
 import Block from './Block';
 import Text from './Text';
 import Button from './Button';
+import Input from './Input';
 
 export {
     Block,
     Text,
     Button,
+    Input,
 };
