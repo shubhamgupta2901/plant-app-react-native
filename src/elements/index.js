45a56b4b95fee24ed2e4925d7fafa7b91410ba83
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-01 23:48:51 +0530

    Added Divider and Switch elements

--- src/elements/index.js
+++ src/elements/index.js
@@ -4,6 +4,8 @@ import Button from './Button';
 import Input from './Input';
 import Card from './Card';
 import Badge from './Badge';
+import Divider from './Divider';
+import Swtich from './Switch';
 import {DotIndicator, BarIndicator} from './indicator';
 
 
@@ -14,6 +16,8 @@ export {
     Input,
     Card,
     Badge,
+    Switch,
+    Divider,
     DotIndicator, 
     BarIndicator,
 };
