e361d17db30d87fae85041324e4c89bf83fc48ae
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-01 23:20:53 +0530

    Added Header to Settings Screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {StyleSheet,View, Text} from 'react-native'
-import PropTypes from 'prop-types'
+import {StyleSheet,ScrollView, Image} from 'react-native';
+import PropTypes from 'prop-types';
+import {Block, Text, Button} from '../elements';
+import {theme,mocks} from '../constants';
+
 
 class SettingsScreen extends React.Component {
   constructor(props){
@@ -8,21 +11,38 @@ class SettingsScreen extends React.Component {
     this.state = {
     }
   }
+  renderHeader = () => {
+    return (
+      <Block flex = {false} center row space="between" style = {styles.header}>
+        <Text h1 bold>Settings</Text>
+        <Button>
+          <Image
+            source = {mocks.profile.avatar}
+            style ={styles.avatar}
+          />
+        </Button>
+      </Block>
+    )
+  }
+
   render(){
     return (
-        <View style={styles.container}>
-            <Text>SettingsScreen</Text>
-        </View>
+        <Block>
+            {this.renderHeader()}
+        </Block>
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    header:{
+      paddingHorizontal: theme.sizes.base*2,
     },
+    avatar: {
+      height: theme.sizes.base*2.4,
+      width: theme.sizes.base*2.4,
+      borderRadius: theme.sizes.base*1.2,
+    }
 })
 
 SettingsScreen.propTypes ={
