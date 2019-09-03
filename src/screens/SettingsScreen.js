280e1a6eefe9d296bf12ab528e455a1c8deb886f
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-03 12:54:50 +0530

    rendered setting inputs dynamically

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import {StyleSheet,ScrollView, Image} from 'react-native';
+import {StyleSheet,ScrollView, Image, View} from 'react-native';
 import PropTypes from 'prop-types';
-import {Block, Text, Button} from '../elements';
+import {Block, Text, Button, Switch, Divider, Input} from '../elements';
 import {theme,mocks} from '../constants';
-
+import {CommonUtils} from '../utils'
 
 class SettingsScreen extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      inputs:{
+        username: {
+          label: 'Username',
+          value: mocks.profile.username,
+          editable: false,
+          hasRightLabel: true,
+        },
+        location: {
+          label: 'Location',
+          value: mocks.profile.location,
+          editable: false,
+          hasRightLabel: true,
+        },
+        email: {
+          label: 'Email',
+          value: mocks.profile.email,
+          editable: false,
+          hasRightLabel: false,
+        }
+    },
     }
   }
+
+  onInputChangeText = (newText, inputKey) =>{
+    if(!this.state.inputs[inputKey])
+      return null;
+
+    let newInputsState = {...this.state.inputs};
+    newInputsState[inputKey].value = newText; 
+    
+    this.setState({inputs: newInputsState});
+  }
+
+  toggleEditable = (inputKey) => {
+    if(!this.state.inputs[inputKey] || !this.state.inputs[inputKey].hasRightLabel)
+      return null;
+    
+    let newInputsState = {...this.state.inputs};
+    newInputsState[inputKey].editable = !this.state.inputs[inputKey].editable; 
+    
+    this.setState({inputs: newInputsState});
+  }
+
   renderHeader = () => {
     return (
       <Block flex = {false} center row space="between" style = {styles.header}>
@@ -25,10 +66,40 @@ class SettingsScreen extends React.Component {
     )
   }
 
+  renderInputs = () => {
+    const inputComponents = [];
+    Object.entries(this.state.inputs).forEach(
+      ([key,value])=>{ inputComponents.push(
+        <Input
+          key = {CommonUtils.generateUniqueId()}
+          value={value.value}
+          label = {value.label}
+          editable = {value.editable}
+          style = {[styles.input, value.editable ?  styles.editableInput : styles.nonEditableInput]}
+          rightLabel = { value.hasRightLabel ? <Text color={theme.colors.primary}>{value.editable? "Save" : "Edit"}</Text> : null}
+          onRightPress = {()=> this.toggleEditable(key)}
+          onChangeText = {(text)=> this.onInputChangeText(text,key)}
+        />
+      )})
+    
+    return (
+      <Block 
+        padding = {[0,theme.sizes.base*2]}
+        color={theme.colors.white}
+      >
+       {inputComponents}
+      </Block>
+    );
+  }
+
   render(){
     return (
         <Block>
-            {this.renderHeader()}
+          {this.renderHeader()}
+          <ScrollView>
+            {this.renderInputs()}
+            <Divider/> 
+          </ScrollView>
         </Block>
     );
   }
@@ -42,10 +113,23 @@ const styles = StyleSheet.create({
       height: theme.sizes.base*2.4,
       width: theme.sizes.base*2.4,
       borderRadius: theme.sizes.base*1.2,
+    },
+    input:{
+      borderColor: "transparent",
+      borderWidth: 0,
+      borderBottomColor: theme.colors.gray2,
+      backgroundColor: theme.colors.white,
+    },
+    editableInput: {
+      fontWeight: '300',
+    },
+    nonEditableInput: {
+      fontWeight: '500',
     }
 })
 
 SettingsScreen.propTypes ={
+
 }
 
 SettingsScreen.defaultProps ={
