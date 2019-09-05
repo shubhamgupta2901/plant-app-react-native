653fc945b0ea62cb05cfe0a751c54f3b93aa31d4
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-05 11:58:23 +0530

    Added rough layout of Products

--- src/screens/ProductsScreen.js
+++ src/screens/ProductsScreen.js
@@ -1,27 +1,92 @@
 import React from 'react';
-import {StyleSheet,View, Text} from 'react-native'
-import PropTypes from 'prop-types'
+import {StyleSheet, ScrollView, Image} from 'react-native';
+import PropTypes from 'prop-types';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Block, Text, Button, Divider} from '../elements';
+import {theme, mocks} from '../constants';
+import {CommonUtils} from '../utils'
 
-const component = (props) => {
-  return(
-    <View style = {styles.container}>
-      <Text>Product Screen</Text>
-    </View>
-  )
+class ProductScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+
+  static navigationOptions = ({navigation}) => {
+    return {
+      headerRight: (
+        <Button>
+          <Icon 
+            color={theme.colors.gray} 
+            size={theme.sizes.base*1.5} 
+            name={"ios-more"} 
+            />
+        </Button>
+      ),
+    };
+  }
+  render(){
+    return (
+      <ScrollView
+        showsVerticalScrollIndicator = {false}
+      >
+        <Block>
+          <Image
+            source = {this.props.product.images[0]}
+            style ={{height: 200}}
+          />
+        </Block>
+        <Block padding = {[theme.sizes.base, theme.sizes.base*2]} >
+          <Text h2 bold> {this.props.product.name}</Text>
+        </Block>
+
+        <Block row  padding = {[0, theme.sizes.base*2]}>
+          {this.props.product.tags.map(tag => (
+            <Button
+              key = {CommonUtils.generateUniqueId()}
+              center
+              color={theme.colors.white} 
+              style = {{
+                marginRight: theme.sizes.base*0.5,
+                borderRadius: theme.sizes.radius* 3 , 
+                borderColor: theme.colors.gray2, 
+                borderWidth: 1,
+                height: theme.sizes.base*2,
+                paddingHorizontal: theme.sizes.base , 
+              }}
+            >
+            <Text small light gray2 center semibold>{tag}</Text>
+           </Button>
+          ))}
+        </Block>
+
+        <Block padding = {[theme.sizes.base, theme.sizes.base*2]} >
+          <Text gray body> {this.props.product.description}</Text>
+        </Block>
+
+        <Divider/>
+        
+        <Block padding = {[0, theme.sizes.base*2]}>
+            <Text bold>Gallery</Text>
+
+        </Block>
+
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+    
 })
 
-component.propTypes ={
+ProductScreen.propTypes ={
+  product : PropTypes.object,
 }
 
-component.defaultProps ={
+ProductScreen.defaultProps ={
+  product: mocks.products[0],
 }
 
-export default component;
+export default ProductScreen;
