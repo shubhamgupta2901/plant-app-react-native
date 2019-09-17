200ec7cd4175ee85c547d0ec19ae262cf4da4266
Shubham17 Gupta <shubham.sg2901@gmail.com>
2019-09-17 17:40:53 +0530

    Reimported unsplash api keys

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+
+/**
+ * NOTE: Rename the file to config.js
+ * Replace YOUR-UNSPLASH_APPLICATION_ID and YOUR-UNSPLASH-SECRET below with your unsplash application_id and secret respectively.
+ * Or register here: https://unsplash.com/join
+ */
+export default config = {
+    UNSPLASH_APPLICATION_ID: 'YOUR-UNSPLASH_APPLICATION_ID',
+    UNSPLASH_SECRET: 'YOUR-UNSPLASH-SECRET',
+};
